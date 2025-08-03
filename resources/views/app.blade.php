b281709bec2bb4d83b234c643abecf7d02e3394a
Ömer İslamoğlu <omer@trinsyca.com>
2025-08-03 09:11:34 +0300

    add script versions from app blade

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,8 +11,8 @@
     @include('layouts.navbar')
     @yield('content')
     @include('layouts.footer')
-    <script src="{{ asset('/scripts/extensions/swiper-bundle.min.js') }}"></script>
-    <script src="{{ asset('/scripts/main.js') }}"></script>
+    <script src="{{ asset('/scripts/extensions/swiper-bundle.min.js') }}?v={{ filemtime(public_path('/scripts/extensions/swiper-bundle.min.js')) }}"></script>
+    <script src="{{ asset('/scripts/main.js') }}?v={{ filemtime(public_path('/scripts/main.js')) }}"></script>
     @yield('scripts')
 </body>
 </html>
